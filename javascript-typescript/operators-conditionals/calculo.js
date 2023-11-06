2a4a9f29f774c06f1dbdb0f968d5e427bf9811f2
Yoxiba <146481264+Yoxiba@users.noreply.github.com>
2023-11-06 20:36:44 +0000

    new files

---
+++ javascript-typescript/operators-conditionals/calculo.js
@@ -0,0 +1,14 @@
+// Change heading:
+
+const prompt = require("prompt-sync")();
+
+const inputX = prompt("Digite o valor de x ");
+const inputY = prompt("Digite o valor de y ");
+
+const x = parseInt(inputX, 10);// o valor 10 é para indicar que é decimal, se nao tiver isto ira da NAN em X9.
+const y = parseInt(inputY, 10);
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
