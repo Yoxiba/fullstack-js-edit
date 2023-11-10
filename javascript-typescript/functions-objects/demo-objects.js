9fbf1d437b3db3fc0771e59ee12faddc5a223e7b
PalmsDev <146481264+Yoxiba@users.noreply.github.com>
2023-11-10 21:25:38 +0000

    t

--- javascript-typescript/functions-objects/demo-objects.js
+++ javascript-typescript/functions-objects/demo-objects.js
@@ -8,7 +8,18 @@ const person = {
 };
 
 // Access properties from object person
+console.log(person.firstName);
 // If a property is undefined, what should I do?
+console.log(person.age);
 // How to invoke a function inside object?
+console.log(person.fullName());
 // How to interact over properties?
+Object.keys(persno).forEach((key) => {
+  console.log(key);
+  console.log(person[key]);
+});
 // How to interact over values?
+
+Object.values(person).forEach((value) => {
+  console.log(value);
+});
